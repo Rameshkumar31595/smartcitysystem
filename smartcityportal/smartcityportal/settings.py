80a82d42387872ae7c4ea55669a37b436afac840
Rameshkumar31595 <2400031595@kluniversity.in>
2026-02-19 23:06:13 +0530

    Switch database to PostgreSQL with environment variable config

--- smartcityportal/smartcityportal/settings.py
+++ smartcityportal/smartcityportal/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import os
 
 from pathlib import Path
 
@@ -70,11 +71,17 @@ WSGI_APPLICATION = "smartcityportal.wsgi.application"
 
 # Database
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'smartcitydb',
+        'USER': 'postgres',
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
+
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
