f2bd6b31d198bbe9fbbe597fac426676045f1915
Rameshkumar31595 <2400031595@kluniversity.in>
2026-02-23 18:24:14 +0530

    refactor: Restore Vercel build properties and fix Lambda constraints

--- smartcityportal/index.py
+++
@@ -1,8 +0,0 @@
-import os
-from django.core.wsgi import get_wsgi_application
-
-# Tell Django where to find the settings file
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "smartcityportal.settings")
-
-# Vercel's Python builder specifically looks for a variable named 'app'
-app = get_wsgi_application()
