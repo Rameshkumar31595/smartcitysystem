ed99d605f9a99db5976c28ae0dbb1cf0d4996c80
Rameshkumar31595 <2400031595@kluniversity.in>
2026-02-25 19:43:06 +0530

    Debug to False

--- smartcityportal/smartcityportal/settings.py
+++ smartcityportal/smartcityportal/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', "django-insecure-d8ms&5dwusg7xvfsx9^sbsdgt*y%(t^jjh58f4b+0e-6^zy2%4")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'True') == 'True'
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
 ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = [
