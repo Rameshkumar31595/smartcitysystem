9958f94f5337c0f4f37ea7ffb28c6313258d460e
Rameshkumar31595 <2400031595@kluniversity.in>
2026-02-25 19:36:48 +0530

    Changes

--- smartcityportal/build.sh
+++ smartcityportal/build.sh
@@ -2,9 +2,6 @@
 # exit on error
 set -o errexit
 
-echo "Installing dependencies..."
-pip install -r requirements.txt
-
 echo "Collecting static files..."
 python manage.py collectstatic --no-input --clear
 
